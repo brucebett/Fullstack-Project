05939e17359d084885cbd4c25d3d213664dc932c
brucebett <bett6448@gmail.com>
2024-10-04 20:24:50 +0300

    fri commit

--- App/urls.py
+++ App/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from App import views
 
 urlpatterns = [
+    path('', views.index,name='index')
+    path('about', views.index,name='about')
+    path('contact', views.index,name='contact')
+
 ]
